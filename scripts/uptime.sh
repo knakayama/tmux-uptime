5c7ecf6fe656bea39e6df6405f94e1f2fc65ad46
knakayama <knakayama.sh@gmail.com>
2015-04-05 22:14:39 +0900

    change

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-uptime | tr -s ' ' | perl -wlne 'print $1 if /(up.+?),/'
+if uptime | grep -qF 'day'; then
+    uptime | tr -s ' ' | perl -wlne 's/\smins//; printf "%s %s min\n",$1,$2 if /up\s(.+?)s?,\s(.+?),/'
+else
+    uptime | tr -s ' ' | perl -wlne 'print $1 if /up\s(.+?),/'
+fi
 
