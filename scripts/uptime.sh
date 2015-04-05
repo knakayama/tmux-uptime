d8700d9b109feaef9062aa972d6de1a567a50a63
knakayama <knakayama.sh@gmail.com>
2015-04-05 22:47:32 +0900

    fix

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -3,6 +3,6 @@
 if uptime | grep -qF 'day'; then
     uptime | tr -s ' ' | perl -wlne 's/\smins//; printf "%s %s min\n",$1,$2 if /up\s(.+?)s?,\s(.+?),/'
 else
-    uptime | tr -s ' ' | perl -wlne 'print $1 if /up\s(.+?),/'
+    uptime | tr -s ' ' | perl -wlne 'printf "$1 min\n"if /up\s(.+?)(:?\smins?)?,/'
 fi
 
