700501ee0b1546438f42ff52bc745ff967727c1b
knakayama <knakayama.sh@gmail.com>
2015-04-24 10:43:09 +0900

    fix regexp

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -3,7 +3,7 @@
 _uptime="$(uptime)"
 
 if echo "$_uptime" | grep -qF 'day'; then
-    echo "$_uptime" | tr -s ' ' | perl -wlne 's/\smins//; printf "%s %s min\n",$1,$2 if /up\s(.+?)s?,\s(.+?),/'
+    echo "$_uptime" | tr -s ' ' | perl -wlne 's/\smins?//; printf "%s %s min\n",$1,$2 if /up\s(.+?)s?,\s(.+?),/'
 elif echo "$_uptime" | grep -qF 'sec'; then
     echo "$_uptime" | tr -s ' ' | perl -wlne 'print $1 if /(\d+\ssecs?)/'
 else
