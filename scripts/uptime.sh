7e0154c9b6b36657c6b4c0a1221ddbab23e224bc
knakayama <knakayama.sh@gmail.com>
2015-05-01 18:56:17 +0900

    a

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -6,7 +6,7 @@ if echo "$_uptime" | grep -qF 'day'; then
   echo "$_uptime" | tr -s ' ' | perl -wlne 's/\smins?//; printf "%sd %sh\n",$1,$2 if /up\s(\d+)\sdays?,\s(.+?),/'
 elif echo "$_uptime" | grep -qF 'sec'; then
   echo "$_uptime" | tr -s ' ' | perl -wlne 'printf "%ss\n",$1 if /(\d+\ssecs?)/'
-elif echo "$_uptime" | grep -qE ':[0-9]{2} mins?'; then
+elif echo "$_uptime" | grep -qE ':[0-9]{2},'; then
   echo "$_uptime" | tr -s ' ' | perl -wlne 'printf "%sh\n",$1 if /up\s(.+?)(:?\smins?)?,/'
 else
   echo "$_uptime" | tr -s ' ' | perl -wlne 'printf "%sm\n",$1 if /up\s(.+?)(:?\smins?)?,/'
