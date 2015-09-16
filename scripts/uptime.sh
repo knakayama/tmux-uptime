73e908be525bf650702e5e2affc71f8000dad3cf
knakayama <knakayama.sh@gmail.com>
2015-09-16 17:56:53 +0900

    Change to short form

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -3,7 +3,7 @@
 _uptime="$(uptime)"
 
 if echo "$_uptime" | grep -qF 'day'; then
-  echo "$_uptime" | tr -s ' ' | perl -wlne 's/\smins?//; printf "%sd %sh\n",$1,$2 if /up\s(\d+)\sdays?,\s(.+?),/'
+  echo "$_uptime" | tr -s ' ' | perl -wlne 's/\smins?//; printf "%sd%sh\n",$1,$2 if /up\s(\d+)\sdays?,\s(.+?),/'
 elif echo "$_uptime" | grep -qF 'sec'; then
   echo "$_uptime" | tr -s ' ' | perl -wlne 'printf "%ss\n",$1 if /(\d+)\ssecs?/'
 elif echo "$_uptime" | grep -qE ':[0-9]{2},'; then
